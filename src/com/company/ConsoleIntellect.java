db82ed186e68e07f312a04c70e71dea7cf6236fb
student1 <tuhta.vlad@mail.ru>
2016-11-21 17:15:32 +0300

    21.11.2016

---
+++ src/com/company/ConsoleIntellect.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by student1 on 21.11.16.
+ */
+public class ConsoleIntellect extends Intellect {
+    @Override
+    public Command decide(int score){
+        return null;
+
+    }
+
+}
---
+++ src/com/company/Intellect.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by student1 on 21.11.16.
+ */
+public abstract class Intellect {
+    public abstract Command decide(int score);
+
+
+}
